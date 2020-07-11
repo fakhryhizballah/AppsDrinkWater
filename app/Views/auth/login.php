8339dd1273b12293ec632bbffe195845ed25c1a5
Fakhry Hizballah <fakhryhizballah@.github.com>
2020-07-11 22:58:08 +0700

    login gagal

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -14,38 +14,50 @@
             <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
             <div class="col-lg">
                 <img src="/img/IG.png" class="logo" alt="">
-                <div class="from-auth">
+                <div class="from-auth user">
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Login!</h1>
                     </div>
-                    <form class="user">
-                        <div class="form-group user-form">
-                            <img class="icon" src="/img/Vector.png" alt="">
-                            <input type="text" class="form-control form-control-user" style="padding-left: 50px;" id="email" name="email" placeholder="Enter Email Address...">
+                    <?php if (!empty(session()->getFlashdata('gagal'))) { ?>
+                        <div class="alert alert-warning">
+                            <?php echo session()->getFlashdata('gagal'); ?>
+                        </div>
+                    <?php } ?>
 
-                        </div>
-                        <div class="form-group user-form">
-                            <img class="icon" src="/img/Group 13.png" alt="">
-                            <input type="password" class="form-control form-control-user" style="padding-left: 50px;" id="password" name="password" placeholder="Password">
-                        </div>
-                        <!-- <div class="form-group">
+                    <?php
+                    echo form_open('login/cek_login')
+                    ?>
+                    <!-- <form class="user"> -->
+                    <div class="form-group user-form">
+                        <img class="icon" src="/img/Vector.png" alt="">
+                        <input type="text" class="form-control form-control-user" style="padding-left: 50px;" id="username" name="username" placeholder="username" required>
+
+                    </div>
+                    <div class="form-group user-form">
+                        <img class="icon" src="/img/Group 13.png" alt="">
+                        <input type="password" class="form-control form-control-user" style="padding-left: 50px;" id="password" name="password" placeholder="Password" required>
+                    </div>
+                    <!-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div> -->
-                        <button type="submit" class="btn btn-user btn-block">
-                            Login
-                        </button>
-                        <p class="font-weight-normal text-right" style="margin-top: 16px;">Forget <strong class="text-primary">Password<strong></p>
+                    <button type="submit" class="btn btn-user btn-block">
+                        Login
+                    </button>
+                    <p class="font-weight-normal text-right" style="margin-top: 16px;">Forget <strong class="text-primary">Password<strong></p>
 
-                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                    <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a> -->
-                    </form>
+                    <!-- </form> -->
+                    <?php
+                    echo form_close()
+                    ?>
                     <!-- <hr>
                     <div class="text-center">
                         <a class="small" href="forgot-password.html">Forgot Password?</a>
