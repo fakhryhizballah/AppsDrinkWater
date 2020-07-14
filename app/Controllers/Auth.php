7d1af2cfb7396848f035988ec3884c1f591761b7
Fakhry Hizballah <fakhryhizballah@.github.com>
2020-07-15 01:10:00 +0700

    validasi regis

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\user_m;
+use App\Models\userModel;
 
 class Auth extends BaseController
 {
@@ -10,6 +11,7 @@ class Auth extends BaseController
 	public function __construct()
 	{
 		$this->authModel = new user_m();
+		$this->userModel = new userModel();
 	}
 	public function index()
 	{
@@ -21,6 +23,7 @@ class Auth extends BaseController
 
 	public function regis()
 	{
+		//session();
 		$data = [
 			'title' => 'Registrasi',
 			'validation' => \Config\Services::validation()
@@ -30,69 +33,84 @@ class Auth extends BaseController
 
 	public function save()
 	{
+		//validasi
+		if (!$this->validate([
+			'aID' => [
+				'rules'  => 'required|is_unique[user.aID]',
+				'errors' => [
+					'required' => 'ID Account wajid di isi',
+					'is_unique' => 'Account sudah terdaftar'
+				]
+			],
+			'nama' => [
+				'rules'  => 'required',
+				'errors' => [
+					'required' => '{field} wajid di isi'
+				]
+			],
+			'cv' => [
+				'rules'  => 'required',
+				'errors' => [
+					'required' => '{field} wajid di isi'
+				]
+			],
+			'email' => [
+				'rules'  => 'required|valid_email|is_unique[user.email]',
+				'errors' => [
+					'required' => '{field} wajid di isi',
+					'valid_email' => 'alamat email tidak benar',
+					'is_unique' => '{field} sudah terdaftar'
+
+				]
+			],
+			'telp' => [
+				'rules'  => 'required|is_natural|is_unique[user.telp]',
+				'errors' => [
+					'required' => 'nomor telpon wajid di isi',
+					'is_natural' => 'nomor telpon tidak benar',
+					//'min_length' => 'nomor telpon tidak valid',
+					'is_unique' => 'nomor telp sudah terdaftar'
+
+				]
+			],
+			'password' => [
+				'rules'  => 'required|min_length[8]',
+				'errors' => [
+					'required' => '{field} wajid di isi',
+					'min_length[8]' => '{field} Minimal 8 karakter'
+
+				]
+			],
+			'password2' => [
+				'rules'  => 'required|matches[password]',
+				'errors' => [
+					'required' => 'password wajid di isi',
+					'matches' => 'password tidak sama'
+
+				]
+			]
+
+		])) {
+			$validation = \config\Services::validation();
+
+			return redirect()->to('/regis')->withInput()->with('validation', $validation);
+		}
 		$data = [
 			'title' => 'Registrasi',
 			'validation' => \Config\Services::validation()
 		];
-		if (!$this->validate([
-			'nama' => [
-				'rules' => 'required|trim',
-				'errors' => [
-					'required' => '{field} harus diisi.',
-				]
-			],
-			'email' => [
-				'rules' => 'required|trim|is_unique[user.email]',
-				'errors' => [
-					'required' => '{field} harus diisi.',
-					'is_unique' => '{field} sudah ada.'
-				]
-			],
-			'telp' => [
-				'rules' => 'required',
-				'errors' => [
-					'required' => '{field} harus diisi.',
-				]
-			],
-			'password' => [
-				'rules' => 'required',
-				'errors' => [
-					'required' => '{field} harus diisi.',
-				]
-			],
-			'password2' => 'required',
-		])) {
-			$validation = \config\Services::validation();
+		// dd($this->request->getVar());
+		$this->userModel->save([
+			'aID' => $this->request->getVar('aID'),
+			'nama' => $this->request->getVar('nama'),
+			'cv' => $this->request->getVar('cv'),
+			'email' => $this->request->getVar('email'),
+			'telp' => $this->request->getVar('telp'),
+			'password' => $this->request->getVar('password')
 
-			$data['validation'] = $validation;
-			// echo view('layout/auth_header');
-			// return redirect()->to('/regis')->withInput()->with('validation', $validation);
-			return view('auth/regis', $data);
-
-			// echo view('layout/auth_footer');
-		} else {
-			// $this->authModel->save([
-			// 	'nama' => $this->request->getVar('nama'),
-			// 	'email' => $this->request->getVar('email'),
-			// 	'telp' => $this->request->getVar('telp'),
-			// 	'image' => 'default.jpg',
-			// 	'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-			// 	'role_id' => '2',
-			// 	'is_active' => 1
-			// ]);
-			$data = [
-				'nama' => $this->request->getVar('nama'),
-				'email' => $this->request->getVar('email'),
-				'telp' => $this->request->getVar('telp'),
-				'image' => 'default.jpg',
-				'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-				'role_id' => '2',
-				'is_active' => 1
-			];
-
-			$this->authModel->insert($data);
-			return redirect()->to('/auth');
-		}
+		]);
+		session()->setFlashdata('Pesan', 'Registration success.');
+		return redirect()->to('/Auth');
 	}
 
 	//--------------------------------------------------------------------
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -18,55 +18,63 @@
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Login!</h1>
                     </div>
-                    <?php if (!empty(session()->getFlashdata('gagal'))) { ?>
-                        <div class="alert alert-warning">
-                            <?php echo session()->getFlashdata('gagal'); ?>
+                    <?php if (session()->getFlashdata('Pesan')) : ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <strong><?= session()->getFlashdata('Pesan');  ?></strong>
                         </div>
-                    <?php } ?>
 
-                    <!-- <form class="user"> -->
-                    <div class="form-group user-form">
-                        <img class="icon" src="/img/Vector.png" alt="">
-                        <input type="text" class="form-control form-control-user" style="padding-left: 50px;" id="username" name="username" placeholder="username" required>
+                        <script>
+                            $(".alert").alert();
+                        </script>
+                    <?php endif; ?>
 
-                    </div>
-                    <div class="form-group user-form">
-                        <img class="icon" src="/img/Group 13.png" alt="">
-                        <input type="password" class="form-control form-control-user" style="padding-left: 50px;" id="password" name="password" placeholder="Password" required>
-                    </div>
-                    <!-- <div class="form-group">
+
+                    <form class="user">
+                        <div class="form-group user-form">
+                            <img class="icon" src="/img/Vector.png" alt="">
+                            <input type="text" class="form-control form-control-user" style="padding-left: 50px;" id="username" name="username" placeholder="username" required>
+
+                            </>
+                            <div class="form-group user-form">
+                                <img class="icon" src="/img/Group 13.png" alt="">
+                                <input type="password" class="form-control form-control-user" style="padding-left: 50px;" id="password" name="password" placeholder="Password" required>
+                            </div>
+                            <!-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div> -->
-                    <button type="submit" class="btn btn-user btn-block">
-                        Login
-                    </button>
-                    <p class="font-weight-normal text-right" style="margin-top: 16px;">Forget <strong class="text-primary">Password<strong></p>
+                            <button type="submit" class="btn btn-user btn-block">
+                                Login
+                            </button>
+                            <p class="font-weight-normal text-right" style="margin-top: 16px;">Forget <strong class="text-primary">Password<strong></p>
 
-                    <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                            <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a> -->
-                    <!-- </form> -->
-                    <!-- <hr>
+                            <!-- </form> -->
+                            <!-- <hr>
                     <div class="text-center">
                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                     </div> -->
-                    <div class="text-center">
-                        <a class="small" href="<?= base_url(); ?>/regis">Create an Account!</a>
-                    </div>
+                            <div class="text-center">
+                                <a class="small" href="<?= base_url(); ?>/regis">Create an Account!</a>
+                            </div>
+                        </div>
                 </div>
             </div>
-        </div>
 
+
+        </div>
 
     </div>
 
-</div>
 
-
-<?= $this->endSection('auth'); ?>
+    <?= $this->endSection('auth'); ?>
