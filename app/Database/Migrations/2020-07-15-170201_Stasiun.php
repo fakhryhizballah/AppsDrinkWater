fbf37fbd7aa39963cd4249eddb8f84fc5c76fdbb
Fakhry Hizballah <fakhryhizballah@.github.com>
2020-07-16 14:29:42 +0700

    Migrations data base

---
+++ app/Database/Migrations/2020-07-15-170201_Stasiun.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Stasiun extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => TRUE,
+				'auto_increment' => TRUE
+			],
+			'id mesin'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '50',
+			],
+			'lokasi'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '225',
+			],
+			'geo'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '225',
+			],
+			'status'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '50',
+			],
+			'isi'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '50',
+			],
+			'indikator'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '50',
+			],
+			'created_at'       => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'updated_at'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+				'null'           => TRUE,
+			],
+
+		]);
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('mesin');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('mesin');
+	}
+}
---
+++ app/Database/Migrations/2020-07-16-060457_admin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Admin extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => TRUE,
+				'auto_increment' => TRUE
+			],
+			'ID akun'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '225',
+			],
+			'nama'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '225',
+			],
+			'email'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+			],
+			'password'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+			],
+			'profil'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+				'null'           => TRUE,
+			],
+			'created_at'       => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'updated_at'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+				'null'           => TRUE,
+			],
+
+		]);
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('admin');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('admin');
+	}
+}
