72e741a383a03994284cb2d17352da3ecb8559f1
Fakhry Hizballah <fakhryhizballah@.github.com>
2020-08-07 22:02:39 +0700

    user update

--- app/Views/layout/user_template.php
+++ app/Views/layout/user_template.php
@@ -36,22 +36,22 @@
         <div class="mt-4 mb-3">
             <div class="row">
                 <div class="col-auto">
-                    <figure class="avatar avatar-60 border-0"><img src="img/user1.png" alt=""></figure>
+                    <figure class="avatar avatar-60 border-0"><img src="/img/user/<?= $akun['profil']; ?>" alt=""></figure>
                 </div>
                 <div class="col pl-0 align-self-center">
-                    <h5 class="mb-1">Ammy Jahnson</h5>
-                    <p class="text-mute small">Work, London, UK</p>
+                    <h5 class="mb-1"><?= $akun['nama']; ?></h5>
+                    <p class="text-mute small"><?= $akun['id_user']; ?></p>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <div class="list-group main-menu">
-                    <a href="/home" class="list-group-item list-group-item-action active"><i class="material-icons icons-raised">home</i>Home</a>
+                    <a href="/user" class="list-group-item list-group-item-action active"><i class="material-icons icons-raised">home</i>Home</a>
 
                     <!-- <a href="notification.html" class="list-group-item list-group-item-action"><i class="material-icons icons-raised">notifications</i>Notification <span class="badge badge-dark text-white">2</span></a> -->
 
-                    <a href="/user/history" class="list-group-item list-group-item-action"><i class="material-icons icons-raised">find_in_page</i>History</a>
+                    <a href="/riwayat" class="list-group-item list-group-item-action"><i class="material-icons icons-raised">find_in_page</i>History</a>
                     <!-- <a href="controls.html" class="list-group-item list-group-item-action"><i class="material-icons icons-raised">view_quilt<span class="new-notification"></span></i>Pages Controls</a> -->
                     <a href="#" class="list-group-item list-group-item-action"><i class="material-icons icons-raised">important_devices</i>Settings</a>
                     <!-- <a href="javascript:void(0)" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#colorscheme"><i class="material-icons icons-raised">color_lens</i>Color scheme</a> -->
