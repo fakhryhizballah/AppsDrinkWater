554e806375e8c36134d17d7f8d44943a5cd6e56d
LuxTenebris99 <gusti.furkanazmi@student.untan.ac.id>
2020-08-12 21:13:32 +0700

    admin

--- app/Views/layout/admin_template.php
+++ app/Views/layout/admin_template.php
@@ -12,12 +12,12 @@
     <title><?= $title; ?></title>
 
     <!-- Custom fonts for this template-->
-    <link href="Asset/sendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="sendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
     <!-- Custom styles for this template-->
-    <link href="Asset/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -264,7 +264,7 @@
                                 </span>
 
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
-                                <!-- <img class="img-profile rounded-circle" src="< ?= base_url('Asset/img/profile/') . $user['image']; ?>"> -->
+                                <!-- <img class="img-profile rounded-circle" src="< ?= base_url('img/profile/') . $user['image']; ?>"> -->
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -326,20 +326,20 @@
         </div>
 
         <!-- Bootstrap core JavaScript-->
-        <script src="Asset/sendor/jquery/jquery.min.js"></script>
-        <script src="Asset/sendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="sendor/jquery/jquery.min.js"></script>
+        <script src="sendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
         <!-- Core plugin JavaScript-->
-        <script src="Asset/sendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="sendor/jquery-easing/jquery.easing.min.js"></script>
 
         <!-- Custom scripts for all pages-->
-        <script src="Asset/js/sb-admin-2.min.js"></script>
+        <script src="js/sb-admin-2.min.js"></script>
 
         <!-- Page level plugins -->
-        <script src="Asset/sendor/chart.js/Chart.min.js"></script>
+        <script src="sendor/chart.js/Chart.min.js"></script>
 
         <!-- Page level custom scripts -->
-        <script src="Asset/js/demo/chart-area-demo.js"></script>
+        <script src="js/demo/chart-area-demo.js"></script>
 
         <!-- Untuk AJAX UBAH DATA -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
--- app/Views/layout/auth_template.php
+++ app/Views/layout/auth_template.php
@@ -13,11 +13,11 @@
     <title><?= $title; ?></title>
 
     <!-- Custom fonts for this template-->
-    <link href="Asset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="Asset/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- my auth.css -->
     <link rel="stylesheet" href="/css/auth.css">
@@ -33,14 +33,14 @@
     <?= $this->renderSection('auth'); ?>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="Asset/vendor/jquery/jquery.min.js"></script>
-    <script src="Asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="Asset/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="Asset/js/sb-admin-2.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
 
     <!-- sweet alert -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
