a55eac97182d6be2b0dba08c0b7a741e41b922bc
fakhryhizballah <falehry88@gmail.com>
2020-11-23 01:57:17 +0700

    menghilangkan  yang tak gune

--- app/Views/layout/admin_template.php
+++ app/Views/layout/admin_template.php
@@ -43,60 +43,6 @@
             <hr class="sidebar-divider">
 
 
-            <!-- QUERY MENU -->
-
-            <!-- < ?php
-            $role_id = $this->session->userdata('role_id');
-            $queryMenu = "SELECT `user_menu`.`id`, `menu`
-                FROM `user_menu` JOIN `user_access_menu` 
-                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
-                WHERE `user_access_menu`.`role_id` = $role_id
-            ORDER BY `user_access_menu`.`menu_id` ASC
-    ";
-            $menu = $this->db->query($queryMenu)->result_array();
-            ?> -->
-
-
-
-            <!-- LOOPING MENU -->
-            <!-- < ?php foreach ($menu as $m) : ?>
-                < ?php if ($m['menu'] != 'Menu') : ?>
-                    <div class="sidebar-heading">
-                        < ?= $m['menu'] ?>
-                    </div> -->
-
-            <!-- SIAPKAN MENU SESUAI SUB-MENU -->
-            <!-- < ?php
-                    $menuId = $m['id'];
-                    $querySubMenu = "SELECT *
-                FROM `user_sub_menu`
-                WHERE `menu_id` = $menuId
-                AND `is_active` = 1
-            ";
-                    $subMenu = $this->db->query($querySubMenu)->result_array();
-                    ?>
-
-                    < ?php foreach ($subMenu as $sm) : ?> -->
-
-            <!-- Nav Item - Dashboard -->
-            <!-- < ?php if ($title == $sm['title']) : ?>
-                            <li class="nav-item active">
-                            < ?php else : ?>
-                            <li class="nav-item">
-                            < ?php endif; ?>
-
-                            <a class="nav-link pb-0" href="< ?= base_url($sm['url']); ?>">
-                                <i class="< ?= $sm['icon']; ?>"></i>
-                                <span>< ?= $sm['title']; ?></span></a>
-                            </li>
-
-                        < ?php endforeach ?> -->
-
-            <!-- Divider -->
-            <!-- <hr class="sidebar-divider mt-3">
-                    < ?php endif; ?>
-                < ?php endforeach; ?> -->
-
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('admin'); ?>" style="color: black;">
                     <i class="fas fa-fw fa-home" style="color: black;"></i>
