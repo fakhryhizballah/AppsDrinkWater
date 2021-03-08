bcde8b53ff2dcb34f4dbc0485516dfeab9f82541
fakhryhizballah <falehry88@gmail.com>
2021-03-08 12:03:40 +0800

    improve user bug

--- app/Views/layout/user_template.php
+++ app/Views/layout/user_template.php
@@ -18,9 +18,9 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Swiper CSS -->
-    <link href="Mandor/swiper/css/swiper.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <!-- <link href="Mandor/swiper/css/swiper.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="Mandor/swiper/css/swiper-bundle.css" />
+    <link rel="stylesheet" href="Mandor/swiper/css/swiper-bundle.min.css" />
 
     <!-- my style.css -->
     <link rel="stylesheet" href="css/style.css">
@@ -235,16 +235,19 @@
 
 
         <!-- jquery, popper and bootstrap js -->
-        <script src="./js/jquery-3.3.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="Mandor/bootstrap-4.4.1/js/bootstrap.min.js"></script>
+        <script src="/js/jquery-3.3.1.min.js"></script>
+        <!-- <script src="Mandor/bootstrap-4.4.1/js/bootstrap.min.js"></script> -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> -->
+        <!-- <script src="js/popper.min.js"></script> -->
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
 
 
         <!-- swiper js -->
+        <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
         <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-        <script src="Mandor/swiper/js/swiper.min.js"></script>
 
         <!-- cookie js -->
         <script src="Mandor/cookie/jquery.cookie.js"></script>
