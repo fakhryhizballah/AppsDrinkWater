7af42f9de3541068d79ad5994836fca8238001d3
fakhryhizballah <falehry88@gmail.com>
2021-05-11 17:31:56 +0800

    mode produk

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -256,7 +256,7 @@ class User extends BaseController
         // \Midtrans\Config::$serverKey = "Mid-server-4i1pIlyNH096QXt7HWHDBT8_";
 
         // Uncomment for production environment
-        \Midtrans\Config::$isProduction = true;
+        \Midtrans\Config::$isProduction = false;
 
         // Enable sanitization
         \Midtrans\Config::$isSanitized = true;
